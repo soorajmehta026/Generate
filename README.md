660026adb42928083a58f611779e428c927fdbf7
soorajmehta026 <87076319+soorajmehta026@users.noreply.github.com>
2023-08-28 23:35:41 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+
+
+```markdown
+# React Rectangles App
+
+The React Rectangles App is a simple web application that allows users to create and manipulate rectangles on a canvas. Users can drag and position rectangles within the canvas, add new rectangles with random sizes and colors, and view the JSON representation of the rectangles.
+
+## Features
+
+- Add rectangles: Click the "Add rectangle" button to add a new rectangle to the canvas. The rectangle will have a random size, position, and color.
+- Drag and position: Click and drag rectangles to reposition them within the canvas. Rectangles are constrained to stay within the canvas boundaries.
+- JSON representation: Click the "Print JSON" button to view the JSON representation of the rectangles in a preformatted section below the canvas.
+- Interactive canvas: The canvas provides an interactive environment for creating, dragging, and managing rectangles.
+
+## Installation and Usage
+
+1. Clone the repository to your local machine:
+
+   ```sh
+   git clone https://github.com/your-username/react-rectangles-app.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```sh
+   cd react-rectangles-app
+   ```
+
+3. Install dependencies using npm:
+
+   ```sh
+   npm install
+   ```
+
+4. Run the application:
+
+   ```sh
+   npm start
+   ```
+
+5. Open your web browser and navigate to [http://localhost:3000] to view the application.
+
+## How to Use
+
+1. Click the "Add rectangle" button to add a new rectangle to the canvas.
+2. Click and drag rectangles to reposition them within the canvas. Rectangles will stay within the canvas boundaries.
+3. Click the "Print JSON" button to view the JSON representation of the rectangles in a preformatted section below the canvas.
+4. To return to the canvas view from the JSON view, simply reload the page.
+
+## Technologies Used
+
+- React: A JavaScript library for building user interfaces.
+- CSS: Styling for the application components.
+
---
+++ src/App.js
@@ -0,0 +1,147 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  // State variables for managing rectangles and interactions
+  const [rectangles, setRectangles] = useState([]); // Array of rectangles
+  const [selectedRectangleIndex, setSelectedRectangleIndex] = useState(null); // Index of the selected rectangle
+  const [draggingRect, setDraggingRect] = useState(null); // Info about the dragging rectangle
+  const [showJson, setShowJson] = useState(false); // Flag to show JSON view
+
+  // Function to display JSON representation when the button is clicked
+  function printRectanglesAsJson() {
+    setShowJson(true);
+  }
+
+  // Function to generate a random color in hexadecimal format
+  function generateRandomColor() {
+    const maxColorValue = 0xFFFFFF;
+    const randomNumber = Math.floor(Math.random() * maxColorValue);
+    const hexColor = randomNumber.toString(16).padStart(6, '0');
+    return `#${hexColor.toUpperCase()}`;
+  }
+
+  // Function to get a random coordinate within a specified range
+  function getRandomCoordinate(maxValue) {
+    return Math.floor(Math.random() * maxValue);
+  }
+
+  // Function to get a random dimension for rectangles
+  function getRandomDimension() {
+    return Math.floor(Math.random() * 10);
+  }
+
+  // Function to add a new rectangle
+  function addRectangle() {
+    const width = getRandomDimension() * 20;
+    const height = getRandomDimension() * 20;
+    const maxYPosition = 580 - height;
+    const newY = Math.min(getRandomCoordinate(maxYPosition), 500 - height);
+    const newRectangle = {
+      x: Math.min(getRandomCoordinate(580 - width), 600 - width),
+      y: Math.max(newY, 100), // Ensure y position is at least 100
+      width: width,
+      height: height,
+      zIndex: rectangles.length + 1,
+      color: generateRandomColor(),
+    };
+
+    // Hide JSON view and update the rectangles array
+    setShowJson(false);
+    setRectangles((prevRectangles) => [...prevRectangles, newRectangle]);
+  }
+
+  // Function to select a rectangle by its index
+  function selectRectangle(index) {
+    setSelectedRectangleIndex(index);
+  }
+
+  // Handle mouse down event for rectangle dragging
+  function handlePointerDown(e, index) {
+    e.preventDefault();
+    const rect = e.target.getBoundingClientRect();
+    const dragOffsetX = e.clientX - rect.left;
+    const dragOffsetY = e.clientY - rect.top;
+    setDraggingRect({ index, dragOffsetX, dragOffsetY });
+    selectRectangle(index);
+  }
+
+  // Handle mouse move event for dragging rectangles
+  function handlePointerMove(e) {
+    if (draggingRect !== null) {
+      const { index, dragOffsetX, dragOffsetY } = draggingRect;
+      const rectWidth = rectangles[index].width;
+      const rectHeight = rectangles[index].height;
+      const canvasWidth = 600;
+      const canvasHeight = 600;
+      
+      // Calculate the maximum allowed positions to keep the rectangle within the canvas
+      const maxX = canvasWidth - rectWidth;
+      const maxY = canvasHeight - rectHeight;
+  
+      // Calculate the new x and y position within the canvas boundaries
+      const newX = Math.max(Math.min(e.clientX - dragOffsetX, maxX), 0);
+      const newY = Math.max(Math.min(e.clientY - dragOffsetY, 700 - rectHeight), 100);
+  
+      // Update the rectangles array with the new position
+      const updatedRectangles = rectangles.map((rectangle, i) =>
+        i === index
+          ? {
+              ...rectangle,
+              x: newX,
+              y: newY,
+            }
+          : rectangle
+      );
+      setRectangles(updatedRectangles);
+    }
+  }
+
+  // Handle mouse up event for ending rectangle dragging
+  function handlePointerUp() {
+    setDraggingRect(null);
+    setSelectedRectangleIndex(null);
+  }
+
+  return (
+    <div className="outer">
+      <div className="buttons">
+        <button onClick={addRectangle}>Add rectangle</button>
+        <button onClick={printRectanglesAsJson}>Print JSON</button>
+      </div>
+      {/* Conditional rendering for JSON view or canvas view */}
+      {showJson ? (
+        <pre className="json-output">{JSON.stringify(rectangles, null, 2)}</pre>
+      ) : (
+        <div
+          className="canvas"
+          style={{ width: '600px', height: '600px', backgroundColor: 'pink' }}
+          onMouseMove={handlePointerMove}
+          onMouseUp={handlePointerUp}
+        >
+          {/* Render rectangles */}
+          {rectangles.map((rectangle, index) => (
+            <div
+              key={index}
+              style={{
+                width: `${rectangle.width}px`,
+                height: `${rectangle.height}px`,
+                backgroundColor: rectangle.color,
+                position: 'absolute',
+                left: `${rectangle.x}px`,
+                top: `${rectangle.y}px`,
+                zIndex: rectangle.zIndex,
+                border: `${
+                  selectedRectangleIndex === index ? '2px solid black' : 'none'
+                }`,
+              }}
+              onMouseDown={(e) => handlePointerDown(e, index)}
+            ></div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
